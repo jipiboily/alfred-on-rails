2a871da45a678f0751e111e7051a2317e8a9c573
jipiboily <jipiboily@gmail.com>
2011-11-27 15:17:17 -0500

    Initial import.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Alfred on Rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlfredOnRails::Application.config.secret_token = '8dee207b1ff3812c165282e39c18b6ad311958cfcf5aec8e20247d3805acd3620e1d904653f5d8f81046ffe71582d186b04b5f8cbf411174baaefd681b700491'
